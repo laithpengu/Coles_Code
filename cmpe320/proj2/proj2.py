2b3a175b5186ff3b8a7d180e2a61d92387a718d7
Cole <colec1@umbc.edu>
2023-03-26 18:54:47 -0400

    proj 2 320

--- cmpe320/proj2/proj2.py
+++ cmpe320/proj2/proj2.py
@@ -15,7 +15,7 @@ import scipy as sp
 
 # Find Values
 PrA = 0.5
-Ntrials = 100000
+Ntrials = 10000
 A_minusA = (np.random.rand(1, Ntrials) <= PrA).astype(int) # 1  = A, 0 = -A
 A_minusA = 2  * (A_minusA - 0.5) # convert to  +/-A
 
@@ -23,6 +23,7 @@ a = 1
 sigma2 = 1/9
 n = np.random.normal(0 , np.sqrt(sigma2), Ntrials)
 r = a * A_minusA + n # R = (+/-A) + N
+r = r.flatten()
 
 x = np.arange(0, Ntrials)
 
@@ -39,9 +40,25 @@ plt.subplots_adjust(hspace = .5)
 axis[0].scatter(x, r)
 axis[0].grid()
 
-# Histogram
-bin_edges = np.arange(-2.5, 5, .125)
+# Histogram and PDF
+bin_edges = np.arange(-2.9375, 3, .125)
 axis[1].hist(r, bins = bin_edges, density = True, edgecolor = "black")
 axis[1].grid()
 
 plt.show()
+
+# Part 2.2: Method 1
+
+# Find S
+s = 2 * np.where(r >= 0, r, 0) # s = kR when R >= 0. k = 2
+
+# Subplot Setup
+fig, axis = plt.subplots(2, 1)
+plt.suptitle("Section 2.2: Method 1")
+plt.subplots_adjust(hspace = .5)
+
+# Scatter test
+axis[0].scatter(r, s)
+
+plt.show()
+
